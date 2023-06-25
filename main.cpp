6417c1a0881604ef863cb89abdab6b1f2323dbf6
RusJJ <mctesting1@yandex.ru>
2023-06-25 20:26:13 +0300

    v1.1 beta
    Fixed configs location (fixes for other than SA games)
    Beta test of write/read string

--- main.cpp
+++ main.cpp
@@ -14,24 +14,52 @@ BEGIN_DEPLIST()
     ADD_DEPENDENCY_VER(net.rusjj.cleolib, 2.0.1.3)
 END_DEPLIST()
 
-#define __decl_op(__name, __int)	const char* NAME_##__name = #__name; const uint16_t OP_##__name = __int;
-#define __reg_opcode				cleo->RegisterOpcode
-#define __reg_func					cleo->RegisterOpcodeFunction
-#define __handler_params 			void *handle, uint32_t *ip, uint16_t opcode, const char *name
-#define __op_name_match(x) 			opcode == OP_##x || strcmp(name, NAME_##x) == 0
-#define __reg_op_func(x, h) 		__reg_opcode(OP_##x, h); __reg_func(NAME_##x, h);
+#define CLEO_RegisterOpcode(x, h) cleo->RegisterOpcode(x, h); cleo->RegisterOpcodeFunction(#h, h)
+#define CLEO_Fn(h) void h (void *handle, uint32_t *ip, uint16_t opcode, const char *name)  
 
-__decl_op(READ_INT_FROM_INI_FILE, 0x0AF0);      // 0AF0=4,%4d% = read_int_from_ini_file %1s% section %2s% key %3s%
-__decl_op(WRITE_INT_TO_INI_FILE, 0x0AF1);       // 0AF1=4,write_int %1d% to_ini_file %2s% section %3s% key %4s%
-__decl_op(READ_FLOAT_FROM_INI_FILE, 0x0AF2);    // 0AF2=4,%4d% = read_float_from_ini_file %1s% section %2s% key %3s%
-__decl_op(WRITE_FLOAT_TO_INI_FILE, 0x0AF3);     // 0AF3=4,write_float %1d% to_ini_file %2s% section %3s% key %4s%
-__decl_op(READ_STRING_FROM_INI_FILE, 0x0AF4);   // 0AF4=4,%4d% = read_string_from_ini_file %1s% section %2s% key %3s%
-__decl_op(WRITE_STRING_TO_INI_FILE, 0x0AF5);    // 0AF5=4,write_string %1s% to_ini_file %2s% section %3s% key %4s%
-
-std::string sGameRoot = "/storage/emulated/0/Android/data/com.rockstargames.gtasa/files/";
+std::string sConfigsRoot;
 static char szConvertedValue[16];
 
-void READ_INT_FROM_INI_FILE(__handler_params)
+inline int GetPCOffset()
+{
+    switch(cleo->GetGameIdentifier())
+    {
+        case GTASA: return 20;
+        case GTALCS: return 24;
+
+        default: return 16;
+    }
+}
+inline char* CLEO_ReadStringEx(void* handle, char* buf, size_t size)
+{
+    uint8_t byte = **(uint8_t**)((int)handle + GetPCOffset());
+    if(byte <= 8) return NULL; // Not a string
+
+    static char newBuf[128];
+    if(!buf || size < 1) buf = (char*)newBuf;
+
+    switch(byte)
+    {
+        case 0xA:
+        case 0xB:
+        case 0x10:
+        case 0x11:
+        {
+            size = (size > 16) ? 16 : size;
+            memcpy(buf, (char*)cleo->GetPointerToScriptVar(handle), size);
+            buf[size-1] = 0;
+            return buf;
+        }
+
+        default:
+        {
+            return cleo->ReadStringLong(handle, buf, size) ? buf : NULL;
+        }
+    }
+    return buf;
+}
+
+CLEO_Fn(READ_INT_FROM_INI_FILE)
 {
     char filename[128], section[64], key[64];
     int result = 0, i = 0;
@@ -43,7 +71,7 @@ void READ_INT_FROM_INI_FILE(__handler_params)
         if(filename[i] == '\\') filename[i] = '/';
         ++i;
     }
-    std::ifstream is((sGameRoot + filename).c_str());
+    std::ifstream is((sConfigsRoot + filename).c_str());
     if(is.is_open())
     {
         ini.parse(is);
@@ -52,7 +80,7 @@ void READ_INT_FROM_INI_FILE(__handler_params)
     cleo->GetPointerToScriptVar(handle)->i = result;
 }
 
-void WRITE_INT_TO_INI_FILE(__handler_params)
+CLEO_Fn(WRITE_INT_TO_INI_FILE)
 {
     char filename[128], section[64], key[64];
     int i = 0, value = cleo->ReadParam(handle)->i;
@@ -64,18 +92,18 @@ void WRITE_INT_TO_INI_FILE(__handler_params)
         if(filename[i] == '\\') filename[i] = '/';
         ++i;
     }
-    std::ifstream is((sGameRoot + filename).c_str());
+    std::ifstream is((sConfigsRoot + filename).c_str());
     if(is.is_open()) ini.parse(is);
     else ini.clear();
 
     sprintf(szConvertedValue, "%d", value);
     ini.sections[section][key] = szConvertedValue;
 
-    std::ofstream os((sGameRoot + filename).c_str());
+    std::ofstream os((sConfigsRoot + filename).c_str());
     if(os.is_open()) ini.generate(os);
 }
 
-void READ_FLOAT_FROM_INI_FILE(__handler_params)
+CLEO_Fn(READ_FLOAT_FROM_INI_FILE)
 {
     char filename[128], section[64], key[64];
     float result = 0.0f; int i = 0;
@@ -87,7 +115,7 @@ void READ_FLOAT_FROM_INI_FILE(__handler_params)
         if(filename[i] == '\\') filename[i] = '/';
         ++i;
     }
-    std::ifstream is((sGameRoot + filename).c_str());
+    std::ifstream is((sConfigsRoot + filename).c_str());
     if(is.is_open())
     {
         ini.parse(is);
@@ -96,7 +124,7 @@ void READ_FLOAT_FROM_INI_FILE(__handler_params)
     cleo->GetPointerToScriptVar(handle)->f = result;
 }
 
-void WRITE_FLOAT_TO_INI_FILE(__handler_params)
+CLEO_Fn(WRITE_FLOAT_TO_INI_FILE)
 {
     char filename[128], section[64], key[64];
     int i = 0; float value = cleo->ReadParam(handle)->f;
@@ -108,19 +136,19 @@ void WRITE_FLOAT_TO_INI_FILE(__handler_params)
         if(filename[i] == '\\') filename[i] = '/';
         ++i;
     }
-    std::ifstream is((sGameRoot + filename).c_str());
+    std::ifstream is((sConfigsRoot + filename).c_str());
     if(is.is_open()) ini.parse(is);
     else ini.clear();
 
     sprintf(szConvertedValue, "%f", value);
     ini.sections[section][key] = szConvertedValue;
 
-    std::ofstream os((sGameRoot + filename).c_str());
+    std::ofstream os((sConfigsRoot + filename).c_str());
     if(os.is_open()) ini.generate(os);
 }
 
 char valRes[100];
-void READ_STRING_FROM_INI_FILE(__handler_params)
+CLEO_Fn(READ_STRING_FROM_INI_FILE)
 {
     char filename[128], section[64], key[64];
     valRes[0] = 0; int i = 0;
@@ -132,37 +160,30 @@ void READ_STRING_FROM_INI_FILE(__handler_params)
         if(filename[i] == '\\') filename[i] = '/';
         ++i;
     }
-    std::ifstream is((sGameRoot + filename).c_str());
+    std::ifstream is((sConfigsRoot + filename).c_str());
     if(is.is_open())
     {
         ini.parse(is);
         sprintf(valRes, "%s", ini.sections[section][key].c_str());
     }
 
-    if(**(uint8_t**)((int)handle + 20) > 8)
+    if(**(uint8_t**)((int)handle + GetPCOffset()) > 8)
     {
         char* dst = (char*)cleo->GetPointerToScriptVar(handle);
         memcpy(dst, valRes, 15); dst[15] = 0;
     }
     else
     {
-        char* dst = (char*)cleo->ReadParam(handle);
+        char* dst = (char*)cleo->ReadParam(handle)->i;
         strcpy(dst, valRes);
     }
 }
 
-void WRITE_STRING_TO_INI_FILE(__handler_params)
+CLEO_Fn(WRITE_STRING_TO_INI_FILE)
 {
-    char filename[128], section[64], key[64], value[128];
+    char filename[128], section[64], key[64], value[128] {0};
     int i = 0;
-    if(**(uint8_t**)((int)handle + 20) > 8) // Is this gonna work..?
-    {
-        cleo->ReadStringLong(handle, value, sizeof(value)); value[sizeof(value)-1] = 0;
-    }
-    else
-    {
-        strcpy(value, (char*)cleo->ReadParam(handle));
-    }
+    CLEO_ReadStringEx(handle, value, sizeof(value));
     cleo->ReadStringLong(handle, filename, sizeof(filename)); filename[sizeof(filename)-1] = 0;
     cleo->ReadStringLong(handle, section, sizeof(section)); section[sizeof(section)-1] = 0;
     cleo->ReadStringLong(handle, key, sizeof(key)); key[sizeof(key)-1] = 0;
@@ -171,13 +192,13 @@ void WRITE_STRING_TO_INI_FILE(__handler_params)
         if(filename[i] == '\\') filename[i] = '/';
         ++i;
     }
-    std::ifstream is((sGameRoot + filename).c_str());
+    std::ifstream is((sConfigsRoot + filename).c_str());
     if(is.is_open()) ini.parse(is);
     else ini.clear();
 
     ini.sections[section][key] = value;
 
-    std::ofstream os((sGameRoot + filename).c_str());
+    std::ofstream os((sConfigsRoot + filename).c_str());
     if(os.is_open()) ini.generate(os);
 }
 
@@ -190,10 +211,13 @@ extern "C" void OnModLoad()
         logger->Error("Cannot load a mod: CLEO's interface is unknown!");
         return;
     }
-    __reg_op_func(READ_INT_FROM_INI_FILE, READ_INT_FROM_INI_FILE);
-    __reg_op_func(WRITE_INT_TO_INI_FILE, WRITE_INT_TO_INI_FILE);
-    __reg_op_func(READ_FLOAT_FROM_INI_FILE, READ_FLOAT_FROM_INI_FILE);
-    __reg_op_func(WRITE_FLOAT_TO_INI_FILE, WRITE_FLOAT_TO_INI_FILE);
-    __reg_op_func(READ_STRING_FROM_INI_FILE, READ_STRING_FROM_INI_FILE);
-    __reg_op_func(WRITE_STRING_TO_INI_FILE, WRITE_STRING_TO_INI_FILE);
+    
+    sConfigsRoot = aml->GetAndroidDataPath(); //cleo->GetCleoStorageDir(); // Nope, this guy cant handle this correctly
+
+    CLEO_RegisterOpcode(0x0AF0, READ_INT_FROM_INI_FILE); // 0AF0=4,%4d% = read_int_from_ini_file %1s% section %2s% key %3s%
+    CLEO_RegisterOpcode(0x0AF1, WRITE_INT_TO_INI_FILE); // 0AF1=4,write_int %1d% to_ini_file %2s% section %3s% key %4s%
+    CLEO_RegisterOpcode(0x0AF2, READ_FLOAT_FROM_INI_FILE); // 0AF2=4,%4d% = read_float_from_ini_file %1s% section %2s% key %3s%
+    CLEO_RegisterOpcode(0x0AF3, WRITE_FLOAT_TO_INI_FILE); // 0AF3=4,write_float %1d% to_ini_file %2s% section %3s% key %4s%
+    CLEO_RegisterOpcode(0x0AF4, READ_STRING_FROM_INI_FILE); // 0AF4=4,%4d% = read_string_from_ini_file %1s% section %2s% key %3s%
+    CLEO_RegisterOpcode(0x0AF5, WRITE_STRING_TO_INI_FILE); // 0AF5=4,write_string %1s% to_ini_file %2s% section %3s% key %4s%
 }
